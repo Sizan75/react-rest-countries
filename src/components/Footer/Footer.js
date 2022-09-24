83c9125728aeb455470700fdbac39981b15624ba
Sizan75 <sakiuzzaman26@gmail.com>
2022-09-24 20:04:58 +0600

    header & footer added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className='bg-dark text-white mb-2 p-5'>Copyright Reserved</footer>
+        </div>
+    );
+};
+
+export default Footer;
