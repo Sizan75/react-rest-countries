7c71b40abaeb95eae9a1a041409ad37b5a2a778a
Sizan75 <sakiuzzaman26@gmail.com>
2022-09-24 18:40:55 +0600

    Country loaded using component

---
+++ src/components/Country/Country.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Country.css';
+
+const Country = (props) => {
+    const {area , population , name, flags} = props.country;
+    console.log(props.country)
+    return (
+        <div className='country'>
+            <h2>Country Name: {name.common}</h2>
+            <img src={flags.png} alt="" />
+            <h5>Population: {population}</h5>
+            <h6>Area: {area}</h6>
+        </div>
+    );
+};
+
+export default Country;
